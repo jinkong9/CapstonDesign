33f48ab8e96eac7fa4e09d21c8797a00f9a58ba6
Jin <kbg08167@naver.com>
2025-10-20 17:13:03 +0900

    중복이메일 구현

--- src/component/Join/join.jsx
+++ src/component/Join/join.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import style from "./join.module.css";
-// import DDlogo from "/DDlogo.webp";
 import { useNavigate } from "react-router-dom";
 import api from "../CreatContextAPI/api";
 
@@ -13,6 +12,9 @@ export default function Join() {
   });
 
   const [agree, setAgree] = useState("");
+  const [emailerr, setEmailerr] = useState(true);
+  const [pwerr, setPwerr] = useState(true);
+  const [checkpwerr, setCheckpwerr] = useState(true);
   const navigate = useNavigate();
 
   const handleJoin = async (e) => {
@@ -58,79 +60,161 @@ export default function Join() {
     }
   };
 
+  useEffect(() => {
+    const Emailhandler = setTimeout(() => {
+      if (!info.email) {
+        setEmailerr(true);
+        return;
+      }
+
+      const checkEmail = async () => {
+        try {
+          await api.get("users/check-email", {
+            params: { email: info.email },
+          });
+          setEmailerr(true);
+        } catch (err) {
+          if (err.response.status === 409) {
+            setEmailerr(false);
+          } else {
+            setEmailerr(true);
+          }
+        }
+      };
+
+      checkEmail();
+    }, 500);
+    return () => {
+      clearTimeout(Emailhandler);
+    };
+  }, [info.email]);
+
+  useEffect(() => {
+    const PWhandler = setTimeout(() => {
+      if (!info.password) {
+        setPwerr(true);
+        return;
+      }
+
+      if (info.password.length < 10) {
+        setPwerr(false);
+      } else {
+        setPwerr(true);
+      }
+    }, 500);
+    return () => {
+      clearTimeout(PWhandler);
+    };
+  }, [info.password]);
+
+  useEffect(() => {
+    const CheckPWhandler = setTimeout(() => {
+      if (!info.passwordconfirm) {
+        setCheckpwerr(true);
+        return;
+      }
+
+      if (info.password !== info.passwordconfirm) {
+        setCheckpwerr(false);
+      } else {
+        setCheckpwerr(true);
+      }
+    }, 500);
+    return () => {
+      clearTimeout(CheckPWhandler);
+    };
+  }, [info.password, info.passwordconfirm]);
+
   return (
     <div className={style.container}>
       <p className={style.joinwrite}>회원가입</p>
       <div>
         <div className={style.joinContainer}>
-        <div>
-          <input
-            className={style.joincon}
-            type="email"
-            id="email"
-            name="emailbox"
-            placeholder="e-mail"
-            value={info.email}
-            onChange={(e) => {
-              setInfo({
-                ...info,
-                email: e.target.value,
-              });
-            }}
-          ></input>
-        </div>
-        <br></br>
-        <div>
-          <input
-            className={style.joincon}
-            type="text"
-            id="name"
-            name="namebox"
-            placeholder="name"
-            value={info.full_name}
-            onChange={(e) => {
-              setInfo({
-                ...info,
-                full_name: e.target.value,
-              });
-            }}
-          ></input>
-        </div>
-        <br></br>
-        <div>
-          <input
-            className={style.joincon}
-            type="password"
-            id="pw"
-            name="pwbox"
-            placeholder="비밀번호"
-            value={info.password}
-            onChange={(e) => {
-              setInfo({
-                ...info,
-                password: e.target.value,
-              });
-            }}
-          ></input>
-        </div>
-        <br></br>
-        <div>
-          <input
-            className={style.joincon}
-            type="password"
-            id="pwconfirm"
-            name="pwbox"
-            placeholder="비밀번호 확인"
-            value={info.passwordconfirm}
-            onChange={(e) => {
-              setInfo({
-                ...info,
-                passwordconfirm: e.target.value,
-              });
-            }}
-          ></input>
-        </div>
-        <br></br>
+          <div>
+            <input
+              className={style.joincon}
+              type="email"
+              id="email"
+              name="emailbox"
+              placeholder="e-mail"
+              value={info.email}
+              onChange={(e) => {
+                setInfo({
+                  ...info,
+                  email: e.target.value,
+                });
+              }}
+            ></input>
+            {emailerr === false ? (
+              <p className={style.warning}>* 이미 사용중인 이메일입니다.</p>
+            ) : (
+              ""
+            )}
+          </div>
+          <br></br>
+          <div>
+            <input
+              className={style.joincon}
+              type="text"
+              id="name"
+              name="namebox"
+              placeholder="name"
+              value={info.full_name}
+              onChange={(e) => {
+                setInfo({
+                  ...info,
+                  full_name: e.target.value,
+                });
+              }}
+            ></input>
+          </div>
+          <br></br>
+          <div>
+            <input
+              className={style.joincon}
+              type="password"
+              id="pw"
+              name="pwbox"
+              placeholder="비밀번호"
+              value={info.password}
+              onChange={(e) => {
+                setInfo({
+                  ...info,
+                  password: e.target.value,
+                });
+              }}
+            ></input>
+            {pwerr === false ? (
+              <p className={style.warning}>
+                * 비밀번호가 짧습니다. 10자 이상 작성해주세요.
+              </p>
+            ) : (
+              ""
+            )}
+          </div>
+          <br></br>
+          <div>
+            <input
+              className={style.joincon}
+              type="password"
+              id="pwconfirm"
+              name="pwbox"
+              placeholder="비밀번호 확인"
+              value={info.passwordconfirm}
+              onChange={(e) => {
+                setInfo({
+                  ...info,
+                  passwordconfirm: e.target.value,
+                });
+              }}
+            ></input>
+            {checkpwerr === false ? (
+              <p className={style.warning}>* 비밀번호가 동일하지않습니다.</p>
+            ) : (
+              ""
+            )}
+          </div>
+          <br></br>
         </div>
         <div className={style.agreement}>
           <div className={style.agreementHeader}>
@@ -159,9 +243,7 @@ export default function Join() {
               </label>
             </div>
           </div>
-          <div className={style.agreementContent}>
-            {/* 기존의 내용을 여기에 유지 */}
-          </div>
+          <div className={style.agreementContent}></div>
         </div>
         <br></br>
         <button className={style.agreebox} onClick={handleJoin}>
